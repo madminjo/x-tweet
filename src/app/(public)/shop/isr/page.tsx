2d2bde1fd76d5c1b1ed72bc10be7a361a34ae7f7
ScrayMadaminjon <madazimovmadaminjonn@gmail.com>
2025-12-11 18:27:40 +0600

    api settings fix

--- src/app/(public)/shop/isr/page.tsx
+++ src/app/(public)/shop/isr/page.tsx
@@ -4,7 +4,7 @@ import { API_URL } from '@/constants'
 // export const revalidate = 300
 
 export default async function ShopISRPage() {
-	const response = await fetch('http://localhost:3000/api/products', {
+	const response = await fetch('http://127.0.0.1:3000/api/products', {
 		next: { revalidate: 300 }
 	})
 console.log(response)
